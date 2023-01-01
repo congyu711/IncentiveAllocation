2714847fe6c834339aed7f11cb99f217e0f2d440
congyu711 <sxlxcsxlxc@gmail.com>
2023-01-01 09:46:32 +0800

    intersection of two linear matroid

--- twofractional/lp.cc
+++ twofractional/lp.cc
@@ -3,159 +3,170 @@
 #include <gurobi_c++.h>
 using namespace std;
 using namespace Eigen;
-int n;  // number of items
-int B;  // budget
+int n; // number of items
+int B; // budget
 
-vector<int> v,c;    // v&c.
-vector<vector<int>> vecs;
-mt19937 gen(time(nullptr));
+vector<int> v, c; // v&c.
+vector<vector<int>> vecs1,vecs2;
+mt19937 gen;
 
 /////////////////////////////////////////////////////////
-//////////////submodular function////////////////////////
-void initvectors(int nrow,int ncol) // each row is a vector
+void initvectors(int nrow, int ncol, vector<vector<int>>& vecs) // each row is a vector
 {
-    vecs.resize(nrow,vector<int>(ncol));
-    for(auto &v:vecs)
+    vecs.resize(nrow, vector<int>(ncol));
+    for (auto &v : vecs)
     {
-        for(auto &e:v)
+        for (auto &e : v)
         {
-            e=gen()%32-16;
+            e = gen() % 32 - 16;
         }
     }
 }
-int getrank(const vector<int>& idx)
+int getrank(const vector<int> &idx, vector<vector<int>>& vecs)
 {
-    MatrixXd m(vecs[0].size(),idx.size());
-    for(int i=0;i<idx.size();i++)
+    MatrixXd m(vecs[0].size(), idx.size());
+    for (int i = 0; i < idx.size(); i++)
     {
-        for(int j=0;j<vecs[0].size();j++)
+        for (int j = 0; j < vecs[0].size(); j++)
         {
-            m(j,i)=vecs[idx[i]][j];
+            m(j, i) = vecs[idx[i]][j];
         }
     }
     FullPivLU<MatrixXd> lu_decomp(m);
     return (int)lu_decomp.rank();
 }
-//////////////submodular function end////////////////////
 /////////////////////////////////////////////////////////
-
-int main()
+class matroidcallback : public GRBCallback
 {
-    n=20;B=155;
-    initvectors(n,n/2);
-    for(int i=0;i<n;i++)
+public:
+    vector<GRBVar> *xs;
+    matroidcallback(vector<GRBVar>* _xs):xs(_xs){}
+protected:
+    void callback()
     {
-        v.push_back(gen()%20+10);
-        c.push_back(gen()%20+10);
-    }
-    /////////////////////////////////////////////////////
-    // algorithm for LD
-
-    double lambda;
-    auto geta=[&](int i){return v[i]-lambda*c[i];};
-    auto cmp=[&](int a,int b){if(geta(a)==geta(b)) return c[a]<c[b];return geta(a)>geta(b);};
-    vector<int> index;
-    for(int i=0;i<n;i++)    index.push_back(i);
-    double opt=1e9;
-    for(int k1=0;k1<n;k1++)
-    {
-        for(int k2=k1;k2<n;k2++)
+        try
         {
-            if(k1==k2)  lambda=1.0*v[k1]/c[k1];
-            else if(c[k1]==c[k2])    continue;
-            else lambda=(v[k1]-v[k2])*1.0/(c[k1]-c[k2])*1.0;
-            if(lambda<0)    continue;
-            sort(index.begin(),index.end(),cmp);
-            if(geta(index[0])<0)    continue;
-            vector<int> fs;
-            vector<int> idx;    // idx for getrank()
-            idx.push_back(index[0]);
-            fs.push_back(getrank(idx));
-            double topt=fs[0]*geta(index[0])+B*lambda;
-            for(int i=1;i<n;i++)
+            if(where==GRB_CB_MIPSOL)
             {
-                if(geta(index[i])<0)    break;
-                idx.push_back(index[i]);
-                fs.push_back(getrank(idx));
-                topt+=(fs[i]-fs[i-1])*geta(index[i]);
+                vector<int> idx;
+                double sumx=0;
+                GRBLinExpr expr=0;
+                vector<double> xval(xs->size());
+                for(int i=0;i<xval.size();i++)
+                {
+                    xval[i]=getSolution(xs->at(i));
+                }
+                for(int i=0;i<xs->size();i++)
+                {
+                    auto c=xval[i];
+                    if(c>0)
+                    {
+                        idx.push_back(i);
+                        sumx+=c;
+                        expr+=xs->at(i);
+                    }
+                }
+                auto r1=getrank(idx,vecs1);
+                auto r2=getrank(idx,vecs2);
+                if(sumx>r1)
+                {
+                    addLazy(expr<=r1);
+                }
+                if(sumx>r2)
+                {
+                    addLazy(expr<=r2);
+                }
             }
-            opt=min(opt,topt);
+        }
+        catch (GRBException e)
+        {
+            cout << "Error number: " << e.getErrorCode() << endl;
+            cout << e.getMessage() << endl;
+        }
+        catch (...)
+        {
+            cout << "Error during callback" << endl;
         }
     }
-    // algorithm for LD end.
-    /////////////////////////////////////////////////////
+};
+int main(int argc, char** argv)
+{
+    // n = 2000;
+    // B = 15500;
+    n=atoi(argv[1]);
+    B=atoi(argv[2]);
+    gen.seed(atoi(argv[3]));
+
+    initvectors(n, n / 2, vecs1);
+    initvectors(n, n / 2, vecs2);
+    for (int i = 0; i < n; i++)
+    {
+        v.push_back(gen() % 2000 + 10);
+        c.push_back(gen() % 200000 + 10);
+    }
+
     try
     {
         // Create an environment
         GRBEnv env = GRBEnv(true);
         // env.set("LogFile", "lp.log");
+        env.set(GRB_IntParam_OutputFlag, 0);
         env.start();
         // Create an empty model
         GRBModel model = GRBModel(env);
+        model.set(GRB_IntParam_LazyConstraints, 1);
         vector<GRBVar> xs;
-        for(int i=0;i<n;i++)
+        for (int i = 0; i < n; i++)
         {
             xs.push_back(model.addVar(0.0, 1.0, 0.0, GRB_CONTINUOUS));
         }
-        GRBLinExpr expr=0,obj=0;
-        for(int i=1;i<(1<<n);i++)
+        GRBLinExpr expr = 0, obj = 0;
+        matroidcallback cb= matroidcallback(&xs);
+        model.setCallback(&cb);
+        // for (int i = 1; i < (1 << n); i++)
+        // {
+        //     vector<int> idx;
+        //     GRBLinExpr submodular = 0;
+        //     bitset<200> bs(i); // !!!
+        //     for (int j = 0; j < n; j++)
+        //     {
+        //         if (bs[j] == 1)
+        //         {
+        //             submodular += xs[j];
+        //             idx.push_back(j);
+        //         }
+        //     }
+        //     model.addConstr(submodular <= getrank(idx));
+        // }
+        for (int i = 0; i < n; i++)
         {
-            vector<int> idx;
-            GRBLinExpr submodular=0;
-            bitset<200> bs(i);  // !!!
-            for(int j=0;j<n;j++)
-            {
-                if(bs[j]==1)    {submodular+=xs[j];idx.push_back(j);}
-            }
-            model.addConstr(submodular<=getrank(idx));
+            expr += c[i] * xs[i];
+            obj += v[i] * xs[i];
         }
-        // GRBLinExpr lin=0;
-        // for(int i=0;i<n;i++)    lin+=xs[i];
-        // model.addConstr(lin<=6);
-        for(int i=0;i<n;i++)    {expr+=c[i]*xs[i];obj+=v[i]*xs[i];}
-        model.addConstr(expr<=B);
-        model.setObjective(obj,GRB_MAXIMIZE);
+        model.addConstr(expr <= B);
+        GRBVar rdt=model.addVar(0.0,1.0,0.0,GRB_INTEGER);
+        obj-=rdt;
+        model.setObjective(obj, GRB_MAXIMIZE);
         model.optimize();
         cout << "Obj: " << model.get(GRB_DoubleAttr_ObjVal) << endl;
-        cout << "LD:  " << opt <<endl;
-        double _B=0;
-        vector<int> fracvar;
+        double sumx=0,_B=0;
+        // vector<int> idxs;
+        vector<double> sol(n);
+        vector<int> frac;
         for(int i=0;i<n;i++)
         {
             _B+=xs[i].get(GRB_DoubleAttr_X)*c[i];
-            if(xs[i].get(GRB_DoubleAttr_X)>1e-6&&xs[i].get(GRB_DoubleAttr_X)<1.0-1e-6)
-            {
-                fracvar.push_back(i);
-            }
+            sol[i]=xs[i].get(GRB_DoubleAttr_X);
+            if(sol[i]!=0 && sol[i]!=1.0)    frac.push_back(i);
+            // sumx+=xs[i].get(GRB_DoubleAttr_X);
+            // if(xs[i].get(GRB_DoubleAttr_X)>0)   idxs.push_back(i);
         }
-        if(fracvar.size()>2||fabs(opt-model.get(GRB_DoubleAttr_ObjVal))>1e-6)
-        {
-            ofstream ferr("CE.out",ios::app);
-            ferr<<n<<'\n';
-            ferr << "Obj: " << model.get(GRB_DoubleAttr_ObjVal) << '\n';
-            ferr << "LD:  " << opt << '\n';
-            for(auto e:vecs)
-            {
-                for(auto ee:e)  ferr<<ee<<' ';
-                ferr<<'\n';
-            }
-            for(auto e:v)   ferr<<e<<' ';ferr<<'\n';
-            for(auto e:c)   ferr<<e<<' ';ferr<<'\n';
-            ferr<<B<<endl;
-            for(auto e:fracvar)
-            {
-                ferr<<'('<<e<<"): "<<xs[e].get(GRB_DoubleAttr_X)<<' ';
-                ferr<<v[e]<<", "<<c[e]<<'\n';
-            }
-            cerr<<"\033[31mlp_opt!=ld_opt\033[0m\n";
-        }
-        cout<<"fracvar: "<<fracvar.size()<<'\n';
-        for(auto e:fracvar)
-        {
-            cout<<'('<<e<<"): "<<xs[e].get(GRB_DoubleAttr_X)<<' ';
-            cout<<v[e]<<", "<<c[e]<<'\n';
-        }
-        cout<<"B/_B: "<<B<<' '<<_B<<'\n';
+        cout<<"_B "<<_B<<endl;
+        // cout<<"sumx "<<sumx<<endl;
+        // cout<<"rank. "<<getrank(idxs)<<endl;
+        cout<<"num of fractional value: "<<frac.size()<<'\n';
+        for(auto e:frac)    cout<<sol[e]<<' ';
+        cout<<endl;
     }
     catch (GRBException e)
     {
--- twofractional/script.py
+++ twofractional/script.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python3
 import os
+import random
 
-while True:
-    os.system("./lp")
+n = 20000
+B = 15500000
+for i in range(25):
+    seed = random.randint(0,123123)
+    print(seed)
+    os.system("./lp "+str(n)+" "+str(B)+" "+str(seed)+" >> lp.out")
+    # input("done\n")
